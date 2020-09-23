40028aeb020693e11934d98cf1011c1fc72c2781
ryan <ryan.jentzsch@gmail.com>
2020-09-23 02:18:45 -0600

    :rocket: Updated the SQL RxChart schema

--- sql/RxChart.sql
+++ sql/RxChart.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `RxChart` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `RxChart`;
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
 --
 -- Host: localhost    Database: RxChart
 -- ------------------------------------------------------
--- Server version	8.0.20
+-- Server version	8.0.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,10 +25,10 @@ DROP TABLE IF EXISTS `MedHistory`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `MedHistory` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `ResidentId` int NOT NULL,
-  `MedicineId` int NOT NULL,
-  `UserId` int NOT NULL,
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `ResidentId` int(11) NOT NULL,
+  `MedicineId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
   `Notes` varchar(500) DEFAULT NULL,
   `Created` timestamp NULL DEFAULT NULL,
   `Updated` timestamp NULL DEFAULT NULL,
@@ -38,7 +40,7 @@ CREATE TABLE `MedHistory` (
   CONSTRAINT `fk_MedHistory_Medicine` FOREIGN KEY (`MedicineId`) REFERENCES `Medicine` (`Id`),
   CONSTRAINT `fk_MedHistory_Resident` FOREIGN KEY (`ResidentId`) REFERENCES `Resident` (`Id`),
   CONSTRAINT `fk_MedHistory_User` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14848 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,18 +51,18 @@ DROP TABLE IF EXISTS `Medicine`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Medicine` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `ResidentId` int DEFAULT NULL,
-  `UserId` int NOT NULL,
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `ResidentId` int(11) DEFAULT NULL,
+  `UserId` int(11) NOT NULL,
   `Drug` varchar(100) NOT NULL,
   `Strength` varchar(20) DEFAULT NULL,
-  `Barcode` varchar(150) NOT NULL,
+  `Barcode` varchar(150) DEFAULT NULL,
   `Directions` varchar(300) DEFAULT NULL,
-  `FillDateMonth` tinyint DEFAULT NULL,
-  `FillDateDay` tinyint DEFAULT NULL,
-  `FillDateYear` int DEFAULT NULL,
+  `FillDateMonth` tinyint(4) DEFAULT NULL,
+  `FillDateDay` tinyint(4) DEFAULT NULL,
+  `FillDateYear` int(11) DEFAULT NULL,
   `Notes` varchar(500) DEFAULT NULL,
-  `OTC` tinyint DEFAULT '0',
+  `OTC` tinyint(4) DEFAULT '0',
   `Created` timestamp NULL DEFAULT NULL,
   `Updated` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
@@ -70,7 +72,7 @@ CREATE TABLE `Medicine` (
   KEY `fk_Medicine_User` (`UserId`),
   CONSTRAINT `fk_Medicine_Resident` FOREIGN KEY (`ResidentId`) REFERENCES `Resident` (`Id`),
   CONSTRAINT `fk_Medicine_User` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1234 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -81,20 +83,20 @@ DROP TABLE IF EXISTS `Resident`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Resident` (
-  `Id` int NOT NULL AUTO_INCREMENT,
-  `UserId` int NOT NULL,
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` int(11) NOT NULL,
   `LastName` varchar(50) NOT NULL,
   `FirstName` varchar(50) NOT NULL,
-  `DOB_YEAR` int DEFAULT NULL,
-  `DOB_MONTH` tinyint DEFAULT NULL,
-  `DOB_DAY` tinyint DEFAULT NULL,
+  `DOB_YEAR` int(11) DEFAULT NULL,
+  `DOB_MONTH` tinyint(4) DEFAULT NULL,
+  `DOB_DAY` tinyint(4) DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   `Created` timestamp NULL DEFAULT NULL,
   `Updated` timestamp NULL DEFAULT NULL,
   UNIQUE KEY `Resident_Id_IDX` (`Id`) USING BTREE,
   KEY `fk_Resident_User` (`UserId`),
   CONSTRAINT `fk_Resident_User` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=292 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,16 +107,16 @@ DROP TABLE IF EXISTS `User`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `User` (
-  `Id` int NOT NULL AUTO_INCREMENT,
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Organization` varchar(100) DEFAULT NULL,
-  `UserName` varchar(30) DEFAULT NULL,
   `PasswordHash` varchar(300) NOT NULL,
   `API_KEY` varchar(100) NOT NULL,
   `Created` timestamp NULL DEFAULT NULL,
   `Updated` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
+  `UserName` varchar(30) DEFAULT NULL,
   UNIQUE KEY `User_Id_IDX` (`Id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -126,4 +128,4 @@ CREATE TABLE `User` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-17  4:46:14
+-- Dump completed on 2020-09-23  1:55:22
