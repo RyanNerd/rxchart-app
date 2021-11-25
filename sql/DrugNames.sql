7a83055225b5df78c432a84dcd5ade3e1edeb5de
ryan <ryan.jentzsch@gmail.com>
2021-11-25 04:59:50 -0700

    ♻ refactor Add UserId to DrugNames create SQL
    - Removed OTC column
    
    Closes #44

--- sql/DrugNames.sql
+++ sql/DrugNames.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `DrugNames` (
     `Id` int NOT NULL AUTO_INCREMENT COMMENT 'List of common drug names',
+    `UserId` int NOT NULL,
     `Drug` varchar(100) NOT NULL,
-    `OTC` tinyint NOT NULL DEFAULT '0',
     `Created` timestamp NULL DEFAULT NULL,
     `Updated` timestamp NULL DEFAULT NULL,
     `deleted_at` timestamp NULL DEFAULT NULL,
     PRIMARY KEY (`Id`),
-    UNIQUE KEY `DrugNames_UNIQUE` (`Drug`,`OTC`)
+    KEY `fk_DrugNames_User` (`UserId`),
+    UNIQUE KEY `DrugNames_UNIQUE` (`Drug`),
+    CONSTRAINT `fk_DrugNames_User` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
