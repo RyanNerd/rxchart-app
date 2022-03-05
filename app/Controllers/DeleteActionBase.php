e3da342452355fddfa505d739cd2f5d7b274014c
ryan <ryan.jentzsch@gmail.com>
2022-03-05 12:58:28 -0700

    ⛲ feat Delete operations can now optionally destroy a record permanently (non-soft delete)
    - Pass a query string of `force=true` to permanently destroy the record

--- app/Controllers/DeleteActionBase.php
+++ app/Controllers/DeleteActionBase.php
@@ -23,13 +23,19 @@ abstract class DeleteActionBase extends ActionBase
     public function __invoke(Request $request, Response $response, array $args): ResponseInterface {
         /** @var ResponseBody $responseBody */
         $responseBody = $request->getAttribute('response_body');
+        $parsedRequest = $responseBody->getParsedRequest();
         $model = $this->model;
+        $id = $args['id'];
+        $model = $model->find($id);
 
-        // Destroy the model given the id.
-        if ($model::destroy($args['id']) === 1) {
-            $status = ResponseCodes::HTTP_OK;
-        } else {
-            $status = ResponseCodes::HTTP_NOT_FOUND;
+        $status = ResponseCodes::HTTP_NOT_FOUND;
+        if ($model !== null) {
+            if (array_key_exists('force', $parsedRequest) && $parsedRequest['force'] === "true") {
+                $isDeleted = $model->forceDelete();
+            } else {
+                $isDeleted = $model->delete();
+            }
+            $status= $isDeleted ? ResponseCodes::HTTP_OK : ResponseCodes::HTTP_INTERNAL_SERVER_ERROR;
         }
 
         // Set the status and data of the ResponseBody
