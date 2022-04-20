648a420a753ed53eed87856b78446acd1f0bf2a7
ryan <ryan.jentzsch@gmail.com>
2022-04-20 01:42:12 -0600

    ⛲ feat Added Recorded TIMESTAMP column to ServiceLog.sql

--- sql/ServiceLog.sql
+++ sql/ServiceLog.sql
@@ -3,8 +3,9 @@ CREATE TABLE `ServiceLog` (
     `UserId` int NOT NULL,
     `ResidentId` int NOT NULL,
     `ServiceId` int NOT NULL,
-    `HmisId` varchar(15) NULL DEFAULT NULL,
-    `Notes` varchar(150) NULL DEFAULT NULL,
+    `HmisId` varchar(15) DEFAULT NULL,
+    `Notes` varchar(150) DEFAULT NULL,
+    `Recorded` timestamp NULL DEFAULT NULL,
     `Created` timestamp NULL DEFAULT NULL,
     `Updated` timestamp NULL DEFAULT NULL,
     `deleted_at` timestamp NULL DEFAULT NULL,
@@ -12,7 +13,7 @@ CREATE TABLE `ServiceLog` (
     KEY `fk_ServiceLog_User` (`UserId`),
     KEY `fk_ServiceLog_Service` (`ServiceId`),
     KEY `fk_ServiceLog_Resident_idx` (`ResidentId`),
-    CONSTRAINT `fk_ServiceLog_Service` FOREIGN KEY (`ServiceId`) REFERENCES `Service` (`Id`),
     CONSTRAINT `fk_ServiceLog_Resident` FOREIGN KEY (`ResidentId`) REFERENCES `Resident` (`Id`),
+    CONSTRAINT `fk_ServiceLog_Service` FOREIGN KEY (`ServiceId`) REFERENCES `Service` (`Id`),
     CONSTRAINT `fk_ServiceLog_User` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
