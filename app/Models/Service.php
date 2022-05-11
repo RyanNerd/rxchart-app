259abae1ea433bb96ae7f53f92d2ba2e3898e4bd
ryan <ryan.jentzsch@gmail.com>
2022-05-11 04:01:40 -0600

    ⛲ feat Service model and table added `AllowMultiple` boolean column

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,10 +6,11 @@ namespace Willow\Models;
 use DateTime;
 
 #[ApplyModelRule(ModelDefaultRules::class)]
-#[ApplyModelColumnAttribute('Id', 'int', null, ['PK', 'NN', 'AI'])] // Service PK
-#[ApplyModelColumnAttribute('UserId', 'int', null, ['NN', 'CE'])]   // User FK
-#[ApplyModelColumnAttribute('HmisId', 'string', 15)]                // HMIS #
-#[ApplyModelColumnAttribute('ServiceName', 'string', 100, ['NN'])]  // Service Name
+#[ApplyModelColumnAttribute('Id', 'int', null, ['PK', 'NN', 'AI'])]         // Service PK
+#[ApplyModelColumnAttribute('UserId', 'int', null, ['NN', 'CE'])]           // User FK
+#[ApplyModelColumnAttribute('HmisId', 'string', 15)]                        // HMIS #
+#[ApplyModelColumnAttribute('ServiceName', 'string', 100, ['NN'])]          // Service Name
+#[ApplyModelColumnAttribute('AllowMultiple', 'boolean', null, ['NN'])]      // Allow multiple services for the same day
 #[ApplyModelColumnAttribute('Created', 'DateTime', null, ['CE'], 'NULL')]
 #[ApplyModelColumnAttribute('Updated', 'DateTime', null, ['CE'], 'NULL')]
 #[ApplyModelColumnAttribute('deleted_at', 'DateTime', null, ['CE'], 'NULL')]
@@ -18,6 +19,7 @@ use DateTime;
  * @property integer $UserId        // User FK
  * @property string $HmisId         // HMIS #
  * @property string $ServiceName    // Service name
+ * @property boolean $AllowMultiple // Allow multiple services for the same day
  * @property DateTime $Created
  * @property DateTime $Updated
  * @property DateTime $deleted_at
--- sql/Service.sql
+++ sql/Service.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `Service` (
     `Id` int NOT NULL AUTO_INCREMENT,
     `UserId` int NOT NULL,
-    `HmisId` varchar(15) NULL DEFAULT NULL,
+    `HmisId` varchar(15) DEFAULT NULL,
     `ServiceName` varchar(100) NOT NULL,
+    `AllowMultiple` tinyint NOT NULL DEFAULT '0',
     `Created` timestamp NULL DEFAULT NULL,
     `Updated` timestamp NULL DEFAULT NULL,
     `deleted_at` timestamp NULL DEFAULT NULL,
